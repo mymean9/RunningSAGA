6d91db7b57c893fb16992f6dd26c0791fc5695bf
HSM\mage3 <mage3@hanmail.net>
2026-04-09 09:57:25 +0900

    fix: manual native registration of background geolocation plugin in MainActivity

--- android/app/src/main/java/com/runningsaga/app/MainActivity.java
+++ android/app/src/main/java/com/runningsaga/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.runningsaga.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.equimaps.capacitor_background_geolocation.BackgroundGeolocation;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(BackgroundGeolocation.class);
+    }
+}
